279ad98e5203ccdbaec1d8231841c383f4ab52a0
GianSandrova <giansandrova16@gmail.com>
2024-10-08 12:50:30 +0700

    tes

--- app/page.js
+++ app/page.js
@@ -74,87 +74,17 @@ export default function Example() {
                   <Logo />
                 </div>
                 <div className="w-1/2 text-center">
-                  <p className="text-md font-bold text-white">Pilih Faskes</p>
+                  <p className="text-md font-bold text-white">Pilihan Faskes Kami</p>
                 </div>
                 <div className="w-1/4"></div>
               </div>
             </section>
-
-            <section className="m-2 mt-15">
-              <div className="relative mt-2 flex justify-center">
-                <div className="w-1/2 relative">
-                  <ListboxButton className="relative w-full cursor-default rounded-md bg-white py-1.5 pl-3 pr-10 text-left text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 sm:text-sm sm:leading-6">
-                    <span className="flex items-center">
-                      <span className="ml-3 block truncate">
-                        {selectedFaskes?.nama_faskes || "Pilih Faskes"}
-                      </span>
-                    </span>
-                    <span className="pointer-events-none absolute inset-y-0 right-0 ml-3 flex items-center pr-2">
-                      <ChevronUpDownIcon
-                        className="h-5 w-5 text-gray-400"
-                        aria-hidden="true"
-                      />
-                    </span>
-                  </ListboxButton>
-
-                  <ListboxOptions className="absolute z-50 mt-1 w-full max-h-56 overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
-                    {faskesOptions.map((faskes) => (
-                      <ListboxOption
-                        key={faskes.id}
-                        className={({ active }) =>
-                          classNames(
-                            active
-                              ? "bg-indigo-600 text-white"
-                              : "text-gray-900",
-                            "relative cursor-default select-none py-2 pl-3 pr-9"
-                          )
-                        }
-                        value={faskes}
-                      >
-                        {({ selected, active }) => (
-                          <>
-                            <div className="flex items-center">
-                              <span
-                                className={classNames(
-                                  selected ? "font-semibold" : "font-normal",
-                                  "ml-3 block truncate"
-                                )}
-                              >
-                                {faskes.nama_faskes}
-                              </span>
-                            </div>
-
-                            {selected ? (
-                              <span
-                                className={classNames(
-                                  active ? "text-white" : "text-indigo-600",
-                                  "absolute inset-y-0 right-0 flex items-center pr-4"
-                                )}
-                              >
-                                <CheckIcon
-                                  className="h-5 w-5"
-                                  aria-hidden="true"
-                                />
-                              </span>
-                            ) : null}
-                          </>
-                        )}
-                      </ListboxOption>
-                    ))}
-                  </ListboxOptions>
-                </div>
-              </div>
-            </section>
-            <section>
-              <div className="flex justify-center pt-2 w-full">
-                <button
-                  type="submit"
-                  onClick={handleClick}
-                  className="bg-green-800 p-1 w-1/2 transition text-center rounded-[5px] font-normal text-[12px] text-white"
-                >
-                  Next
-                </button>
-              </div>
+            <section className="m-2 mt-4">
+              <Search
+                onChange={(e) => setSearch(e.target.value)}
+                value={search}
+                placeholder="Cari Faskes"
+              />
             </section>
             {/* <section className="flex justify-center">
             <div className="p-1 font-bold text-lg text-center text-black">
